7a6ffb384ee7854ddb8f251fc1dd1d92d1bdbb6c
Raul Arribas <raul@exordo.com>
2018-08-16 18:12:51 +0100

    Some tweaks in configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname,
-
-  entry: {
-    index: './public/javascripts/index.js',
-    index_two: './public/javascripts/index_2.js',
-  },
-
-  // devtool: 'inline-source-map',
+  context: path.resolve(__dirname, 'public/'),
+  entry: './javascripts/index.js',
 
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/dist')
-  },
-
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
   },
 };
